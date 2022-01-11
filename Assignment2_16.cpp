29b2c1d4adaf7373916cc91b657be639f8304c02
Mrunali Kubadge <95906908+MrunaliKubadge@users.noreply.github.com>
2022-01-12 00:22:57 +0530

    Add files via upload

---
+++ Assignment2_16.cpp
@@ -0,0 +1,173 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Account
+{
+	private:
+	struct ACC
+	{
+		double accNo;
+	    char name[20];
+	    float balance;
+	}ACC[10];
+	public:
+	int ch;
+	int index=0;
+	void getChoice();
+	void show(int index);
+	void openAccount();
+	void viewAccount();
+	void depositAmt();
+	void withdrawAmt();
+	void viewBalance();
+	void searchAcc();
+	
+};
+
+int main()
+{
+	Account a;
+	a.getChoice();
+	return 0;
+}
+
+void Account::getChoice()
+{
+	while(1)
+	{
+	    cout<<"\n1.Open Account";
+        cout<<"\n2.View Account";
+        cout<<"\n3.Deposit Amount";
+        cout<<"\n4.Withdraw Amount";
+        cout<<"\n5.View Balance";
+        cout<<"\n6.Search Account";
+        cout<<"\n7.Exit";
+        cout<<"\nEnter Choice:";
+        cin>>ch;
+    switch(ch)
+    {
+    	case 1:
+    	    Account::openAccount();
+    		break;
+    	case 2:
+    		Account::viewAccount();
+    		break;
+    	case 3:
+			Account::depositAmt();
+			break;
+		case 4:
+			Account::withdrawAmt();
+			break;
+		case 5:
+			Account::viewBalance();
+			break;
+		case 6:
+			Account::searchAcc();
+			break;
+		case 7:
+			cout<<"\nYou have terminated Process...!";
+			exit(0);
+	}
+	
+	}
+}
+
+void Account::openAccount()
+{
+	cout<<"\nEnter Account Number:";
+	cin>>ACC[index].accNo;
+	cout<<"\nEnter Account holder's Name: ";
+	cin>>ACC[index].name;
+	cout<<"\nEnter balance to be deposited:";
+	cin>>ACC[index].balance;
+    index++;
+}
+
+void Account::viewAccount()
+{
+	double userip;
+	cout<<"\nEnter Account number:";
+    cin>>userip;
+    for(int i=0;i<index;i++)
+    {
+    	if(ACC[i].accNo==userip)
+    	{
+    		Account::show(i);
+    		break;
+		}
+		else
+		cout<<"\nInvalid Account Number......!";
+    }
+}
+
+void Account::depositAmt()
+{
+	double userip;
+	float depAmt;
+	cout<<"\nEnter Account Number";
+	cin>>userip;
+	for(int i=0;i<index;i++)
+	{
+		if(ACC[i].accNo==userip)
+		{
+			ACC[i].balance=ACC[i].balance+depAmt;
+			Account::show(i);
+		}
+	}
+}
+
+void Account::withdrawAmt()
+{
+    double userip;
+	float wdAmt;
+	cout<<"\nEnter Account Number";
+	cin>>userip;
+	for(int i=0;i<index;i++)
+	{
+		if(ACC[i].accNo==userip)
+		{
+			ACC[i].balance=ACC[i].balance-wdAmt;
+			Account::show(i);
+		}
+	}
+}
+
+void Account::viewBalance()
+{
+	double userip;
+	cout<<"\nEnter Account Number";
+	cin>>userip;
+	for(int i=0;i<index;i++)
+	{
+		if(ACC[i].accNo==userip)
+		{
+			cout<<"\nYour Account Balance:"<<ACC[i].balance;
+		}
+	} 
+}
+
+void Account::searchAcc()
+{
+	double userip;
+	cout<<"\nEnter Account Number";
+	cin>>userip;
+	for(int i=0;i<index;i++)
+	{
+		if(ACC[i].accNo==userip)
+		{
+			cout<<"\nAccount Details are found:";
+			Account::show(i);
+		}
+	} 
+	
+}
+
+void Account::show(int index)
+{
+    cout<<"\nAccount Number:"<<ACC[index].accNo;
+    cout<<"\nAccount holder's Name:"<<ACC[index].name;
+    cout<<"\nAccount Balance:Rs."<<ACC[index].balance;
+}
+
+
--- Assignment2_21.cpp
+++ Assignment2_21.cpp
@@ -3,7 +3,6 @@ Input 3 Entries and display the details. The Program can also useful to find the
 Enquiry from particular City.*/
 #include<iostream>
 #include<string.h>
-#include<iomanip>
 using namespace std;
 
 class Enquiry
@@ -11,40 +10,61 @@ class Enquiry
 	public:
     int enqNo;
 	char name[20],city[10],date[15];
-	double phnNo;
-	void addEnquiry(int enqNo,char name[],char city[],double phnNo,char date[]);
+	char phnNo[20];
+	void addEnquiry(int enqNo,char name[],char city[],char phnNo[],char date[]);
 	void display();
 };
 
+Enquiry findbyCity(Enquiry e[])
+{
+	char userip[20];
+	cout<<"\nEnter city Name:";
+	cin>>userip;
+	for(int i=0;i<3;i++)
+	{
+		if(strcmpi(userip,e[i].city)==0)
+		{
+			return e[i];
+		    break;
+		}
+		else 
+		return e[-1];
+	}
+}
+
 int main()
 {
-	Enquiry e1,e2,e3;
+	Enquiry e[3];
 	
-	e1.addEnquiry(652,"Jason Wood","London",6119856754,"25/8/2006");
-	e2.addEnquiry(82,"Lily Collins","California",6522498756,"2/6/2004");
-	e3.addEnquiry(459,"Cheryl Kingsley","Moscow",5648915756,"6/12/2018");
+	e[0].addEnquiry(652,"Jason Wood","London","6119856754","25/8/2006");
+	e[1].addEnquiry(82,"Lily Collins","California","6522498756","2/6/2004");
+	e[0].addEnquiry(459,"Cheryl Kingsley","Moscow","5648915756","6/12/2018");
 	
-	e1.display();
-	e2.display();
-	e3.display();
+	for(int i=0;i<3;i++)
+	{
+		e[i].display();
+	}
+	Enquiry ec=findbyCity(e);
+	ec.display();
 	return 0;
 }  
 
-void Enquiry:: addEnquiry(int enqNo,char name[],char city[],double phnNo,char date[])
+void Enquiry:: addEnquiry(int enqNo,char name[],char city[],char phnNo[],char date[])
 {
 	this->enqNo=enqNo;
 	strcpy(this->name,name);
 	strcpy(this->city,city);
-	this->phnNo=phnNo;
+	strcpy(this->phnNo,phnNo);
 	strcpy(this->date,date);
 }
 void Enquiry::display()
 {
-	cout.setf(ios::right);
-	cout<<setw(5)<<"\n"<<this->enqNo
-	<<setw(15)<<"\t"<<this->name
-	<<setw(10)<<"\t"<<this->city
-	<<setw(15)<<"\t"<<this->phnNo
-	<<setw(15)<<"\t"<<this->date;
+	cout<<"\n"<<this->enqNo
+	<<"\t"<<this->name
+	<<"\t"<<this->city
+	<<"\t"<<this->phnNo
+	<<"\t"<<this->date;
 } 
+
+
 
